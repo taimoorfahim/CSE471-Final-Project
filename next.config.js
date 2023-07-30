abb616c0308f39bf1a40e2eaff4d03eb0ce82f95
taimoorfahim <85748122+taimoorfahim@users.noreply.github.com>
2023-07-30 13:47:02 +0600

    file added

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "i.imgur.com",
+            "imgur.com",
+            "img.freepik.com",
+        ],
+    },
+}
+
+module.exports = nextConfig
