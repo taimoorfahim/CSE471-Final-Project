8c8d7bd599f4918a5a42a1be902180a3f4c4dcd5
taimoorfahim <taimoor.fahim@g.bracu.ac.bd>
2023-08-13 02:37:50 +0600

    Error solved

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 import { ClerkProvider } from '@clerk/nextjs'
 import QueryProvider from '@/utils/providers/QueryProvider'
 import { Toaster } from '@/components/ui/Toast'
-import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'Rs utility tool',
