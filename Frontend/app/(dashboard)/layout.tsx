e547d7024b8fa8e4f53672c7f2648b8e79b73f9a
Ananda221B <kazi.shahed.mamun@g.bracu.ac.bd>
2023-08-13 06:53:36 +0600

    Error of pull request fixed

--- Frontend/app/(dashboard)/layout.tsx
+++ Frontend/app/(dashboard)/layout.tsx
@@ -7,13 +7,12 @@ const Dashboardlayout = async ({
 }: {
     children: React.ReactNode
 }) => {
-    // const apiLimitCount = await getApiLimitCount();
-    const apiLimitCount = 0
+   
 
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <Sidebar apiLimitCount={apiLimitCount} />
+                <Sidebar/>
             </div>
             <main className="md:pl-72 bg-gray-800 h-full">
                 <Navbar />
